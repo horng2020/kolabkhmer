bb6b9ea99142a2c1b18bf30fcc17c99a0314f19c
horng2020 <56008358+horng2020@users.noreply.github.com>
2019-12-08 19:12:23 +0700

    update

--- k.js
+++ k.js
@@ -125,7 +125,7 @@ for(var i=0; i<titles.length; i++){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("merlhdkh")>-1){
 		html_ += 'fa fa-film';
-	}else if(files[i].indexOf("")>-1){
+	}else if(files[i].indexOf("blogspot")>-1){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("kolab-kh")>-1){
 		html_ += 'fa fa-film';

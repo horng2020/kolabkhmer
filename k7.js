8cb1ca85762037ea6c3c69ba525ac4ffa35a39e8
horng2020 <56008358+horng2020@users.noreply.github.com>
2019-12-16 15:51:03 +0700

    fa

--- k7.js
+++ k7.js
@@ -144,7 +144,7 @@ for(var i=0; i<titles.length; i++){
 	}else if(files[i].indexOf("mp4upload")>-1){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("blogger")>-1){
-		html_ += 'fa fa-film';
+		html_ += 'fa fa-folder-open-o';
 	}else if(files[i].indexOf("pivdo")>-1){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("kolabkhmer")>-1){

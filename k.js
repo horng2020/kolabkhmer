a92634d55f9a1f2fea4dc88a8923145138b2505d
horng2020 <56008358+horng2020@users.noreply.github.com>
2019-12-16 15:52:06 +0700

    fa

--- k.js
+++ k.js
@@ -130,7 +130,7 @@ for(var i=0; i<titles.length; i++){
 	}else if(files[i].indexOf("blogspot")>-1){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("blogger")>-1){
-		html_ += 'fa fa-film';
+		html_ += 'fa fa-folder-open-o';
 	}else if(files[i].indexOf("mp4upload")>-1){
 		html_ += 'fa fa-film';
 	}else if(files[i].indexOf("kolab-khmer")>-1){

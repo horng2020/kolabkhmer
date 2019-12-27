4cb9e9a0773ec4498ec5f82aa083bc4a220dfa80
horng2020 <56008358+horng2020@users.noreply.github.com>
2019-12-27 20:29:44 +0700

    Create playwebblock.js

---
+++ playwebblock.js
@@ -0,0 +1,51 @@
+
+    if(document.referrer.indexOf("https://movie-khmer.com/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://www.phumi7.net") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://phumikhmer1.com") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://www.khmer-drama.com") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://www.komsanmovie.com") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://phumikhmer11.com") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://khmermovies.club") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://www.drama68.com/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://www.khmerdrama.com/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://phumi7.top") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://khmerdubbed.club") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    } 
+    
+    if(document.referrer.indexOf("http://thaidrama.club") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://chdrama.com/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("https://phumdrama.com/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("http://thailakornhd.top/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    if(document.referrer.indexOf("https://th7.club/") != -1) {
+        window.location = "http://www.kolabkhmer7.club/";
+    }
+    

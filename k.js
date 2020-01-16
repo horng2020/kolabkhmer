110501a5741162414903bea9821c1817c8fb501b
horng2020 <56008358+horng2020@users.noreply.github.com>
2020-01-16 14:35:09 +0700

    up

--- k.js
+++ k.js
@@ -138,9 +138,9 @@ for(var i=0; i<titles.length; i++){
 	}else if(files[i].indexOf("solidfiles")>-1){
 		html_ += 'fa-sellsy';
 	}else if(files[i].indexOf("kolabkhmer")>-1){
-		html_ += 'fa-folder-open';
-	}else if(files[i].indexOf("mp4upload")>-1){
 		html_ += 'fa-heart';
+	}else if(files[i].indexOf("mp4upload")>-1){
+		html_ += 'fa fa-film';
 	}
         else{
 		html_ += 'fa-file';

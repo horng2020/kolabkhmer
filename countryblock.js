cc41b6953cdc9f42222f0565efe7c74af40f8682
horng2020 <56008358+horng2020@users.noreply.github.com>
2020-02-01 07:01:43 +0700

    countryblock

---
+++ countryblock.js
@@ -0,0 +1,14 @@
+if (geoplugin_countryCode() == "KH") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to Khmer -->
+        } else if (geoplugin_countryCode() == "TH") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to Thailand -->
+        } else if (geoplugin_countryCode() == "KR") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to Korea -->
+        } else if (geoplugin_countryCode() == "CH") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to China -->
+        } else if (geoplugin_countryCode() == "VN") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to Vietnam -->
+        } else if (geoplugin_countryCode() == "IN") {
+            window.location.href = "//kolab-khmer.com"    <!-- This directs to India -->
+        } 
+		
